636886b7fff0bd0066a86e21009db7c82a93e534
DmitriySalnikov <salnikov.mine@yandex.ru>
2024-01-22 00:20:12 +0300

    Removed the x64 platform in favor of Any CPU. Added a link to `PCK Bruteforcer` to the encryption key input window.

--- Explorer/UI/Forms/OpenWithPCKEncryption.Designer.cs
+++ Explorer/UI/Forms/OpenWithPCKEncryption.Designer.cs
@@ -31,6 +31,7 @@
             label1 = new Label();
             btn_ok = new Button();
             tb_key = new TextBoxWithPlaceholder();
+            linkLabel1 = new LinkLabel();
             SuspendLayout();
             // 
             // label1
@@ -45,10 +46,10 @@
             // 
             // btn_ok
             // 
-            btn_ok.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            btn_ok.Location = new Point(12, 56);
+            btn_ok.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn_ok.Location = new Point(12, 73);
             btn_ok.Name = "btn_ok";
-            btn_ok.Size = new Size(424, 27);
+            btn_ok.Size = new Size(455, 27);
             btn_ok.TabIndex = 4;
             btn_ok.Text = "Confirm";
             btn_ok.UseVisualStyleBackColor = true;
@@ -60,20 +61,33 @@
             tb_key.CueBanner = "04bf1d.., 04 BF 1D.., 04-BF-1D..";
             tb_key.Location = new Point(12, 27);
             tb_key.Name = "tb_key";
-            tb_key.Size = new Size(424, 23);
+            tb_key.Size = new Size(455, 23);
             tb_key.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(225, 55);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(242, 15);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Don't know the key? Try the PCK Bruteforcer.";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
             // 
             // OpenWithPCKEncryption
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(448, 89);
+            ClientSize = new Size(479, 106);
+            Controls.Add(linkLabel1);
             Controls.Add(tb_key);
             Controls.Add(btn_ok);
             Controls.Add(label1);
             Margin = new Padding(4, 3, 4, 3);
-            MaximumSize = new Size(1192, 128);
-            MinimumSize = new Size(338, 128);
+            MaximumSize = new Size(1192, 145);
+            MinimumSize = new Size(338, 145);
             Name = "OpenWithPCKEncryption";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Enter the encryption key";
@@ -86,5 +100,6 @@
         private Label label1;
         private Button btn_ok;
         private TextBoxWithPlaceholder tb_key;
+        private LinkLabel linkLabel1;
     }
 }
